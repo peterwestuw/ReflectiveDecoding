75f51ad773fe84f19a280478aa6a0d5d70d069ff
Peter <peterwestuw>
2021-06-10 13:09:31 -0700

    fixing data loading

--- data/get_data.sh
+++ data/get_data.sh
@@ -2,6 +2,7 @@ mkdir quora
 
 wget https://storage.googleapis.com/ai2-mosaic/public/abductive-commonsense-reasoning-iclr2020/anli.zip 
 unzip anli.zip 
+rm anli.zip
 
 wget http://qim.fs.quoracdn.net/quora_duplicate_questions.tsv
 cp quora_duplicate_questions.tsv quora
