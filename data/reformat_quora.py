96dcc78ccd3943c2de649db1d2b2d9b4e1b4e25a
Peter <peterwestuw>
2021-06-10 13:12:17 -0700

    fix data

--- data/reformat_quora.py
+++ data/reformat_quora.py
@@ -1,4 +1,7 @@
 with open('quora/quora_duplicate_questions.tsv') as f_in, open('quora/quora_questions.tsv','w') as f_out:
     for line in f_in:
-        source, target = line.split('\t')[2],line.split('\t')[3]
+        try:
+            source, target = line.split('\t')[3],line.split('\t')[4]
+        except:
+            print(line)
         f_out.write( '{}\t{}\n'.format(source,target))
