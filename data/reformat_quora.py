04185e305356b9201afda346435153a16688672c
Peter <peterwestuw>
2021-06-10 13:07:24 -0700

    fixed quora

--- data/reformat_quora.py
+++ data/reformat_quora.py
@@ -1,4 +1,4 @@
-with open('quora_duplicate_questions.tsv') as f_in, open('quora_duplicate_questions.tsv','w') as f_out:
+with open('quora/quora_duplicate_questions.tsv') as f_in, open('quora/quora_duplicate_questions.tsv','w') as f_out:
     for line in f_in:
         source, target = line.split('\t')[2],line.split('\t')[3]
         f_out.write( '{}\t{}\n'.format(source,target))
