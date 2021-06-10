2f275013a3626a444650bd0948f9a09802e22e92
Peter <peterwestuw>
2021-06-10 13:16:31 -0700

    fixed quora

--- data/reformat_quora.py
+++ data/reformat_quora.py
@@ -1,7 +1,14 @@
 with open('quora/quora_duplicate_questions.tsv') as f_in, open('quora/quora_questions.tsv','w') as f_out:
-    for line in f_in:
+    for i, line in enumerate(f_in):
+        
+        ## skip the first line (banner)
+        if i == 0:
+            continue
+            
+            
         try:
             source, target = line.split('\t')[3],line.split('\t')[4]
         except:
+            print('FAILED)
             print(line)
         f_out.write( '{}\t{}\n'.format(source,target))
